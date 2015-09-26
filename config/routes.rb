94fc187ab337dc883e040baaf119d85f2a1c6751
Afan <afan.khan50@gmail.com>
2015-09-26 19:29:23 -0400

    Add Seller page

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :listings
+  
   get 'pages/about'
-
   get 'pages/contact'
+  get 'seller' => "listings#seller"
 
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
-
-  # You can have the root of your site routed with "root"
-    root 'listings#index'
+  root 'listings#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
